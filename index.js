dd92e206a7326da06b23a4db160327a287199c6c
thakur-deepak <deepakthakur@founderandlightning.com>
2022-11-11 12:55:25 +0530

    new push

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const {request, response} = require("express");
 const app = express()
 const db = require('./postgres')
-const port = 3004
+const port =  process.env.port || 5000
 
 app.use(bodyParser.json())
 app.use(
