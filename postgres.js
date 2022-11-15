9b324751f21cb863d7c96378ceeeeb0366cfa826
demo-tester-dpk <deepakthakur@founderandlightning.com>
2022-11-15 09:22:30 +0530

    updated postgres

--- postgres.js
+++ postgres.js
@@ -46,8 +46,24 @@ const updateUser = (request, response) => {
     )
 }
 
+const deleteUser = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query(
+        'DELETE FROM users WHERE id = $1',
+        [id],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`User Deleted with ID: ${id}`)
+        }
+    )
+}
+
 module.exports = {
     getUsers,
     createUser,
-    updateUser
+    updateUser,
+    deleteUser
 }
