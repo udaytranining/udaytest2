6a0e6f9da9ea7f756ddf4c908645b426fe36f4de
udaytranining <116781700+udaytranining@users.noreply.github.com>
2022-12-27 20:25:51 +0530

    latest change

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
-import express from 'express';
-import { json, urlencoded } from 'body-parser';
-import { request, response } from "express";
+const express = require('express');
+const bodyParser = require('body-parser');
+const {request, response} = require("express");
 const app = express();
-import { getUsers, getUsersSale, createUser, createUserSalesforce, updateUser, deleteUser } from './postgres';
+const db = require('./postgres');
 const port = process.env.PORT || 5557;
 
-
-app.use(json())
+app.use(bodyParser.json())
 app.use(
-    urlencoded({
+    bodyParser.urlencoded({
         extended:true,
     })
 )
@@ -18,14 +17,13 @@ app.get('/', (request, response) => {
         response.json({info: 'Our class is not live.'})
 })
 
-app.get('/users', getUsers)
-app.get('/users/sales', getUsersSale)
-app.post('/users', createUser)
-app.post('/users/sales', createUserSalesforce)
-app.put('/users/:id', updateUser)
-app.delete('/users/:id', deleteUser)
+app.get('/users', db.getUsers)
+app.get('/users/sales', db.getUsersSale)
+app.post('/users', db.createUser)
+app.post('/users/sales', db.createUserSalesforce)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(port,() => {
     console.log(`App is running on port ${port}.`)
 })
-
